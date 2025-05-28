505f37b0d407a2f4b22201c2af43faa22b3784ba
Vasanth <vasanthaj739@gmail.com>
2025-05-28 16:49:51 +0530

    initial commit of local project

---
+++ src/main/java/com/example/face_recognition_attendance_projects/FaceRecognitionAttendanceProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.face_recognition_attendance_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionAttendanceProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionAttendanceProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/face_recognition_attendance_projects/FaceRecognitionAttendanceProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.face_recognition_attendance_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionAttendanceProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
