381f56245694a298c5a4dfb1f24a6373c06b2d1e
kishan-raj-ravi <kishanraj0912@gmail.com>
2025-05-30 20:06:48 +0530

    Resolved merge confllict and update serivce file

--- src/main/java/com/example/face_recognition_attendance_projects/Employees/controller/EmployeesController.java
+++ src/main/java/com/example/face_recognition_attendance_projects/Employees/controller/EmployeesController.java
@@ -1,6 +1,7 @@
 package com.example.face_recognition_attendance_projects.Employees.controller;
 
 import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,51 +10,45 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.face_recognition_attendance_projects.Employees.dto.EmployeesRequestDTO;
 import com.example.face_recognition_attendance_projects.Employees.entity.Employees;
 import com.example.face_recognition_attendance_projects.Employees.service.EmployeesService;
-import com.example.face_recognition_attendance_projects.Users.entity.Users;
-
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeesController {
 
 	private EmployeesService employeesService;
-	
-	    public EmployeesController(EmployeesService employeesService) {
-		super();
+
+	public EmployeesController(EmployeesService employeesService) {
+
 		this.employeesService = employeesService;
 	}
 
-	    @PostMapping("/{userId}")
-	    public Employees addEmployee(@RequestBody EmployeesRequestDTO requestDTO,
-	                                 @PathVariable Long userId) {
-	        return employeesService.createEmployee(requestDTO, userId);
-	    }
+	@PostMapping("/{userId}")
+	public Employees addEmployee(@RequestBody EmployeesRequestDTO requestDTO, @PathVariable Long userId) {
+		return employeesService.createEmployee(requestDTO, userId);
+	}
 
+	@GetMapping
+	public List<Employees> getAllEmployees() {
+		return employeesService.getAllEmployees();
+	}
 
-	    @GetMapping
-	    public List<Employees> getAllEmployees() {
-	        return employeesService.getAllEmployees();
-	    }
+	@GetMapping("/{id}")
+	public Employees getEmployeeById(@PathVariable Long id) {
+		return employeesService.getEmployeeById(id);
+	}
 
-	    @GetMapping("/{id}")
-	    public Employees getEmployeeById(@PathVariable Long id) {
-	        return employeesService.getEmployeeById(id);
-	    }
-	    
-	    @PutMapping("/{id}")
-	    public Employees updateEmployee(@PathVariable Long id,@RequestBody EmployeesRequestDTO requestDTO) {
-	    	return employeesService.updateEmployee(id, requestDTO);
-	    }
+	@PutMapping("/{id}")
+	public Employees updateEmployee(@PathVariable Long id, @RequestBody EmployeesRequestDTO requestDTO) {
+		return employeesService.updateEmployee(id, requestDTO);
+	}
 
-	    @DeleteMapping("/{id}")
-	    public void deleteEmployee(@PathVariable Long id) {
-	        employeesService.deleteEmployee(id);
-	    }
-	    
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable Long id) {
+		employeesService.deleteEmployee(id);
+	}
+
 }
-	
-
-
