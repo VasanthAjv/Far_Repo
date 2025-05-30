aa29e2f58a1497f755643b4c41062e0766729581
kishan-raj-ravi <kishanraj0912@gmail.com>
2025-05-30 20:00:45 +0530

    Resolved merge conflict in UsersController.java and manager module

--- src/main/java/com/example/face_recognition_attendance_projects/Users/controller/UsersController.java
+++ src/main/java/com/example/face_recognition_attendance_projects/Users/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.example.face_recognition_attendance_projects.Users.controller;
 
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.face_recognition_attendance_projects.Users.dto.UserRequestDTO;
 import com.example.face_recognition_attendance_projects.Users.entity.Users;
 import com.example.face_recognition_attendance_projects.Users.service.UserService;
@@ -17,39 +19,44 @@ import com.example.face_recognition_attendance_projects.Users.service.UserServic
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-	
+
 	private UserService userService;
 
 	public UsersController(UserService userService) {
+
 		this.userService = userService;
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Users> createUser(@RequestBody UserRequestDTO userRequestDTO){	
-		Users user=userService.createUser(userRequestDTO);	
-		return ResponseEntity.ok(user);	
+//	 @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER', 'HR')")
+	public ResponseEntity<Users> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+		Users user = userService.createUser(userRequestDTO);
+		return ResponseEntity.ok(user);
 	}
 
-    @GetMapping
-    public ResponseEntity<List<Users>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
-    }
+	@GetMapping
+//	@PreAuthorize("hasAnyRole('ADMIN', 'MANAGER', 'HR')")
+	public ResponseEntity<List<Users>> getAllUsers() {
+		return ResponseEntity.ok(userService.getAllUsers());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Users> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
-    }
+	@GetMapping("/{id}")
+//	 @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER', 'HR')")
+	public ResponseEntity<Users> getUserById(@PathVariable Long id) {
+		return ResponseEntity.ok(userService.getUserById(id));
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO requestDTO) {
-        Users updatedUser = userService.updateUser(id, requestDTO);
-        return ResponseEntity.ok(updatedUser);
-    }
+	@PutMapping("/{id}")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER', 'HR')")
+	public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO requestDTO) {
+		Users updatedUser = userService.updateUser(id, requestDTO);
+		return ResponseEntity.ok(updatedUser);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return ResponseEntity.ok("User deleted successfully");
-    }
-    
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+		return ResponseEntity.ok("User deleted successfully");
+	}
+
 }
--- src/main/java/com/example/face_recognition_attendance_projects/Users/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/face_recognition_attendance_projects/Users/service/impl/UserServiceImpl.java
@@ -2,36 +2,41 @@ package com.example.face_recognition_attendance_projects.Users.service.impl;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.face_recognition_attendance_projects.FaceRecognitionAttendanceProjectsApplication;
 import com.example.face_recognition_attendance_projects.Users.dto.UserRequestDTO;
 import com.example.face_recognition_attendance_projects.Users.entity.Users;
+import com.example.face_recognition_attendance_projects.Users.entity.Users.Role;
 import com.example.face_recognition_attendance_projects.Users.exception.UserNotFoundException;
 import com.example.face_recognition_attendance_projects.Users.repository.UserRepository;
 import com.example.face_recognition_attendance_projects.Users.service.UserService;
 
-
 @Service
 public class UserServiceImpl implements UserService {
 
+	private final FaceRecognitionAttendanceProjectsApplication faceRecognitionAttendanceProjectsApplication;
+
 	private UserRepository userRepository;
-	
-	public UserServiceImpl(UserRepository userRepository) {
-		super();
+
+	public UserServiceImpl(UserRepository userRepository,
+			FaceRecognitionAttendanceProjectsApplication faceRecognitionAttendanceProjectsApplication) {
+
 		this.userRepository = userRepository;
+		this.faceRecognitionAttendanceProjectsApplication = faceRecognitionAttendanceProjectsApplication;
 	}
 
 	@Override
 	public Users createUser(UserRequestDTO userRequestDTO) {
-		
-		Users users=Users.builder()
-				.userName(userRequestDTO.getUserName())
-				.password(userRequestDTO.getPassword())
-				.email(userRequestDTO.getEmail())
-				.mobileNo(userRequestDTO.getMobileNo())
-				.role(userRequestDTO.getRole())
-				.createdAt(LocalDateTime.now())
-				.build();
-		
+		if (userRequestDTO.getRole() != Role.ADMIN || userRequestDTO.getRole() != Role.MANAGER
+				|| userRequestDTO.getRole() != Role.HR) {
+			throw new UserNotFoundException("Invalid role: " + userRequestDTO.getRole());
+		}
+		Users users = Users.builder().userName(userRequestDTO.getUserName()).password(userRequestDTO.getPassword())
+				.email(userRequestDTO.getEmail()).mobileNo(userRequestDTO.getMobileNo()).role(userRequestDTO.getRole())
+				.createdAt(LocalDateTime.now()).build();
+
 		return userRepository.save(users);
 	}
 
@@ -40,30 +45,36 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findAll();
 	}
 
-	@Override
+	@Override // etha method ku security apo condition kudukanum (/id vandha permit nu)
 	public Users getUserById(Long id) {
-	    Users user=userRepository.findById(id).orElseThrow(()->new UserNotFoundException("User not found with Id : "+id));
+
+		Users user = userRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException("User not found with Id : " + id));
 		return user;
 	}
 
 	@Override
 	public Users updateUser(Long id, UserRequestDTO userRequestDTO) {
-	
-		Users user=getUserById(id);
-		
+		if (userRequestDTO.getRole() != Role.ADMIN || userRequestDTO.getRole() != Role.MANAGER
+				|| userRequestDTO.getRole() != Role.HR) {
+			throw new UserNotFoundException("Invalid role: " + userRequestDTO.getRole());
+		}
+		Users user = getUserById(id);
+
 		user.setUserName(userRequestDTO.getUserName());
 		user.setPassword(userRequestDTO.getPassword());
 		user.setEmail(userRequestDTO.getEmail());
 		user.setMobileNo(userRequestDTO.getMobileNo());
 		user.setRole(userRequestDTO.getRole());
-		
+
 		return userRepository.save(user);
 	}
 
 	@Override
 	public void deleteUser(Long id) {
-		Users user=getUserById(id);
-		
+
+		Users user = getUserById(id);
+
 		userRepository.delete(user);
 
 	}
